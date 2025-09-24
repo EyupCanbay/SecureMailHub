6a86c94db01a9bb8bb81410e338103d1e105061c
EyupCanbay <eyupcanbay40@gmail.com>
2025-09-24 22:42:59 +0300

    Added mail service

---
+++ mail-service/cmd/api/handlers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var requestPayload mailMessage
+
+	log.Println("readjson öncesi")
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println("readjson sonrası")
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	err = app.Mailer.SendSMTPMEssage(msg)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println("send smtp sonrası")
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "sent to " + requestPayload.To,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
